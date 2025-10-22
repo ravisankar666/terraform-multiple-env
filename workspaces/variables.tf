2ff01f6af351c42fe129aa542440f2fdcefa8e93
ravisankar666 <ravisankarp666@gmail.com>
2025-10-23 01:12:56 +0530

    some message

---
+++ workspaces/variables.tf
@@ -0,0 +1,60 @@
+variable "project" {
+    default = "roboshop"
+  
+}
+
+variable "environment" {
+    type = map
+    default = {
+        dev = "dev"
+        prod = "prod"
+    }
+    
+}
+variable "ami_id" {
+    type = string
+    default     = "ami-09c813fb71547fc4f"
+}
+
+
+variable "instance_type" {
+    type = map
+    default = {
+        dev = "t3.micro"
+        prod = "t3.small"
+    }
+  
+}
+variable "sg_name" {
+    type = string
+    default = "allow-alls"
+    #optional to inform what is this variable about
+    description = "Security group name to attach to ec2 instance"
+  
+}
+
+variable "cidr" {
+    type = list  
+    default = ["0.0.0.0/0"]
+}
+
+variable "ingress_from_port" {
+    default = 0
+}
+
+variable "ingress_to_port" {
+    default = 0
+}
+
+variable "egress_from_port" {
+    default = 0
+}
+
+variable "egress_to_port" {
+    default = 0
+}
+
+variable "protocol" {
+    type = string
+    default = "-1"
+}
